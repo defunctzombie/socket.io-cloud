9c6b29eeb8a06bc2c7c7139dc1f8faa467e14fd5
nickguo <nick.n.guo@gmail.com>
2015-01-10 10:55:33 -0800

    initial working cloud server

--- app.js
+++ app.js
@@ -1,9 +1,86 @@
 // Setup basic express server
 var express = require('express');
 var bodyParser = require('body-parser');
-var pubsub = require('./lib/pubsub');
 var debug = require('debug')('chat');
 //var EventEmitter = require('events').EventEmitter;
+
+// now needed by 'app'-----------
+var SocketIO = require('socket.io');
+var io = new SocketIO();
+
+var namespaces = [
+    io.of('/ns1'),
+    io.of('/ns2'),
+    io.of('/ns3')
+];
+
+for (i in namespaces) {
+    namespaces[i].on('connection', handleConnection(namespaces[i]));
+}
+
+function handleConnection(ns) {
+    return function (socket) {
+        console.log('connected');
+
+      // when the client emits 'new message', this listens and executes
+      socket.on('new message', function (data) {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+          username: socket.username,
+          message: data
+        });
+      });
+
+      // when the client emits 'add user', this listens and executes
+      socket.on('add user', function (username) {
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+          numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+          username: socket.username,
+          numUsers: numUsers
+        });
+      });
+
+      // when the client emits 'typing', we broadcast it to others
+      socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+          username: socket.username
+        });
+      });
+
+      // when the client emits 'stop typing', we broadcast it to others
+      socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+          username: socket.username
+        });
+      });
+
+      // when the user disconnects.. perform this
+      socket.on('disconnect', function () {
+        // remove the username from global usernames list
+        if (addedUser) {
+          delete usernames[socket.username];
+          --numUsers;
+
+          // echo globally that this client has left
+          socket.broadcast.emit('user left', {
+            username: socket.username,
+            numUsers: numUsers
+          });
+        }
+      });
+    }
+}
+
+//-------------------------------
 
 // var ee = new EventEmitter();
 
@@ -13,20 +90,23 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 
-// API example -------------------------------------
-
 app.use(bodyParser.json());
 
-app.post('/api/messages', function(req,res) {
+app.post('/update', function(req,res) {
   var body = req.body;
-  pubsub.emit('message', body.message);
-  debug('finish ee emit');
+  var id = body.id;
+  var msg = body.message;
+
+  // TODO:error checks for invalid id, empty id, empty request
+
+  // TODO: fix hardcode
+  var nsp = namespaces[0];
+  nsp.emit('new message', msg);
+  console.log('new message was: ' + msg);
 
   res.json({
     message: body.message
   });
 });
-
-//--------------------------------------------------
 
 module.exports = app;
