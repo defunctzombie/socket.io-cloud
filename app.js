349c47f55ae080c4884dad9751431a12bc7ec93a
nickguo <nick.n.guo@gmail.com>
2015-01-10 16:40:56 -0800

    CHECKPOINT: abstracted app.js into (app, io, namespace, and bin/cloud)

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
 // Setup basic express server
 var express = require('express');
 var bodyParser = require('body-parser');
-var debug = require('debug')('chat');
-
-
-// now needed by 'app'-----------
-var SocketIO = require('socket.io');
-var io = new SocketIO();
-
-var namespace_list = ['ns1', 'ns2', 'ns3'];
-
-var namespaces = {};
-
-namespace_list.forEach( function(namespace) {
-    namespaces[namespace] = io.of('/'+namespace);
-    namespaces[namespace].on('connection', handleConnection(namespaces[namespace]));
-});
-
-function handleConnection(ns) {
-    return function (socket) {
-        console.log('connected');
-    }
-}
-
-//-------------------------------
-
-// var ee = new EventEmitter();
+var debug = require('debug')('cloud');
+var namespaces = require('./namespaces');
 
 var app = express();
 
 // Routing -> serve static files from public folder
 app.use(express.static(__dirname + '/public'));
-
 
 app.use(bodyParser.json());
 
@@ -41,34 +17,17 @@ app.post('/update', function(req,res) {
   var type = body.type;
   var msg = body.message;
 
-  console.log('id was: ' + id);
+  debug('id was: ' + id);
 
   // TODO:error checks for invalid id, empty id, empty request
 
   var nsp = namespaces[id];
   nsp.emit(type, msg);
-  console.log(type + 'action had payload: ' + msg);
+  debug(type + ' action had payload: ' + msg);
 
   res.json({
-    message: msg
+    payload: msg
   });
 });
 
-//module.exports = {SocketIOCloud: io, app: app};
-//module.exports = app;
-
-var debug = require('debug')('chat');
-//var io = require('../io');
-
-var http = require('http');
-
-var server = http.createServer(app);
-io.attach(server);
-var port = process.env.PORT || 3000;
-
-server.listen(port, function () {
-  debug('Server listening at port %d', port);
-});
-
-module.exports = io;
-console.log("done loading app.js");
+module.exports = app;
---
+++ namespaces.js
@@ -0,0 +1,19 @@
+var io = require('./io');
+var debug = require('debug')('cloud');
+
+var namespace_list = ['ns1', 'ns2', 'ns3'];
+
+var namespaces = {};
+
+namespace_list.forEach( function(namespace) {
+    namespaces[namespace] = io.of('/'+namespace);
+    namespaces[namespace].on('connection', handleConnection(namespaces[namespace]));
+});
+
+function handleConnection(ns) {
+    return function (socket) {
+        debug('connected');
+    }
+}
+
+module.exports = namespaces;
